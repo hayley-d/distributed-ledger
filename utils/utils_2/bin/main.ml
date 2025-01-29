c95faeb23f39a6bae26e292d5ce0b72b1fcb0943
hayley-d <hayleydod@proton.me>
2025-01-29 15:34:52 +0200

    adding utility

---
+++ utils/utils_2/bin/main.ml
@@ -0,0 +1,9 @@
+(* Function that counts how many elements in a list are equal to a given value *)
+let count_elem lst x = 
+    let rec aux acc = function
+    | [] -> acc
+    | h :: t -> if h = x then aux (acc + 1) t else aux acc t 
+    in
+    aux 0 lst
+
+let () = count_elem [1;2;1;3;1] 1
