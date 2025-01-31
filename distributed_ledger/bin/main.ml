ff646a7522319688b8f643fbde442bc7f794525f
hayley-d <hayleydod@proton.me>
2025-01-31 02:58:25 +0200

    added some more functions

--- distributed_ledger/bin/main.ml
+++ distributed_ledger/bin/main.ml
@@ -9,8 +9,27 @@
 }
 
 
-    let compute_hash index prev_hash timestamp data nonce =
-        let input = Printf.sprintf "%d%s%f%s%f%d" index prev_hash timestamp data nonce 
-        in to_hex (digest_string input)
+let compute_hash index prev_hash timestamp data nonce =
+    let input = Printf.sprintf "%d%s%f%s%f%d" index prev_hash timestamp data nonce 
+    in to_hex (digest_string input)
 
-let create_block index
+let create_block index prev_hash data nonce = 
+    let timestamp = Unix.gettimeofday () in
+    let hash = compute_hash index prev_hash timestamp data nonce in
+    {index;prev_hash = prev_hash;timestamp;data;nonce;hash}
+
+let genesis_block = create_block 0 "0" "Genesis Block" 0
+
+let add_block chain data difficulty =
+    let last_block = List.hd chain in
+    let timestamp = Unix.gettimeofday () in
+    let nonce, hash = validate_hash (last_block.index + 1) last_block.hash timestamp data 0 difficulty in
+    let new_block = {index = last_block.index + 1;previous_hash = last_block.hash; timestamp; data;nonce;hash} in
+    new_block :: chain
+
+(* Verify blockchain integrity *)
+let rec is_valid_chain = function
+        |[] | [_] -> true
+        | a :: b :: tail -> if a.previous_hash = b.hash && is_valid_chain (b :: tail) then true else false
+
+
