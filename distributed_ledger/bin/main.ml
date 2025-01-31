359c526073993836c48a25c5308e1e08b6f9843a
hayley-d <hayleydod@proton.me>
2025-01-31 03:01:55 +0200

    added some tests

--- distributed_ledger/bin/main.ml
+++ distributed_ledger/bin/main.ml
@@ -9,17 +9,21 @@
 }
 
 
+(* Compute the hash of a block *)
 let compute_hash index prev_hash timestamp data nonce =
     let input = Printf.sprintf "%d%s%f%s%f%d" index prev_hash timestamp data nonce 
     in to_hex (digest_string input)
 
+(* Create a new block *)
 let create_block index prev_hash data nonce = 
     let timestamp = Unix.gettimeofday () in
     let hash = compute_hash index prev_hash timestamp data nonce in
     {index;prev_hash = prev_hash;timestamp;data;nonce;hash}
 
+(* Generate the genesis block *)
 let genesis_block = create_block 0 "0" "Genesis Block" 0
 
+(* Add a new block to the chain *)
 let add_block chain data difficulty =
     let last_block = List.hd chain in
     let timestamp = Unix.gettimeofday () in
@@ -32,4 +36,8 @@ let rec is_valid_chain = function
         |[] | [_] -> true
         | a :: b :: tail -> if a.previous_hash = b.hash && is_valid_chain (b :: tail) then true else false
 
-
+let () = 
+    let blockchain = [genesis_block] in
+    let blockchain = add_block blockchain "Block 1" 4 in
+    let blockchain = add_block blockchain "Block 2" 4 in
+    List.iter (fun block -> Printf.printf "Block %d: %s\n" block.index block.hash) blockchain
