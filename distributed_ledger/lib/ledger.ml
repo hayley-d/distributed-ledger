1fb595211f4c57ff0347fa258db26751c09bff7e
hayley-d <hayleydod@proton.me>
2025-02-01 13:28:45 +0200

    Update dag related functions

--- distributed_ledger/lib/ledger.ml
+++ distributed_ledger/lib/ledger.ml
@@ -1,51 +1,26 @@
-module StringMap = Map.Make(String)
-
-type ledger = {
-    transactions : transaction StringMap.t; (* Store transactions by ID *)
-    references : (string list) StringMap.t; (* Track child transactions *)
+type transaction = {
+    id : string;
+    data: string;
+    signature: string;
+    mutable chit: bool;
+    mutable confidence: float;
+    mutable consecutive_success: int;
 }
 
-(* Initialize an empty ledger *)
-let empty_ledger = {
-    transactions = StringMap.empty;
-    references = StringMap.empty;
+type vertex = {
+    id: string;
+    transaction: transaction;
+    parents: vertex list;
+    mutable accepted: bool;
 }
 
-(* Add a transaction to the DAG *)
-let add_transaction ledger tx =
-    let transactions = StringMap.add tx.id tx ledger.transactions in
-    let references = List.fold_left (fun acc parent_id ->
-        let updated_children =
-            match StringMap.find_opt parent_id acc with
-            | Some chidren -> tx.id :: children
-            | None -> [tx.id]
-        in
-        StringMap.add parent_id updated_children acc
-    ) ledger.references tx.parents in 
-    { transactions; references}
 
-(* Check if a transaction exists *)
-let transaction_exists ledger tx_id =
-    StringMap.mem tx_id ledger.transactions
+type dag = {
+    mutable verticies : (string, vertex) Hashtbl.t 
+}
 
-(* Get a transaction by ID *)
-let get_transaction ledger tx_id =
-    StringMap.find_opt tx_id ledger.transactions
+let add_vertex dag vertex =
+    Hashtbl.add dag.verticies vertex.id vertex
 
-(* Get parent transactions *)
-let get_parents ledger tx =
-    List.filter_map (fun id -> get_transaction ledger id) tx.parents
-
-(* Get child transactions *)
-let get_children ledger tx_id =
-    match StringMap.find_opt tx_id ledger.references with
-    | Some children -> List.filter_map (fun id -> get_transaction ledger id) children
-    | None -> []
-
-
-(* Perform a traversal for validation *)
-let validate_transaction ledger tx =
-    List.for_all (transaction_exists ledger) tx.parents
-
-
-
+let get_vertex dag id =
+    Hashtbl.find_opt dag.verticies id
